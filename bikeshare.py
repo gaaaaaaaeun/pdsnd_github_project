ea9375099bbac4479d039deb8d9028053281d61f
Gaeun Kang <cookie1227@naver.com>
2021-08-03 23:59:08 +0900

    bikeshare.py : Change of the code of for statement of chi data2

--- bikeshare.py
+++ bikeshare.py
@@ -213,7 +213,7 @@ chi <- data.frame(chi, Hour)
 C_Time <- function(data){
     time = 1
     each_time = NULL
-    for (num in 1:24){
+    for num in range(1:24){
         each_time[time] = length(which(hour(as.POSIXct(data$Start.Time, format="%Y-%m-%d %H")) == time))
         time = time + 1
     }
